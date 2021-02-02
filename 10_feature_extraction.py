72a4aaad6cd67eb9fa5a0dfaba41ce305664b12f
pelka-izabella <60815087+pelka-izabella@users.noreply.github.com>
2021-02-03 00:15:58 +0100

    saved file

--- 10_feature_extraction.py
+++ 10_feature_extraction.py
@@ -1,12 +1,15 @@
 #%% 
 from text_prepper import PrepareText
+import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 # %%
 ## Setup
 in_dir = 'data'
@@ -45,7 +48,33 @@ bow = CountVectorizer(max_features=1000, lowercase=True, ngram_range=(1,1),analy
 bow_trans = bow.fit_transform(df['lem']).toarray()
 bow_trans
 # %%
-X_train, X_test, y_train, y_test = train_test_split(bow_trans, labels, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X=bow_trans, y=labels, test_size=0.2, random_state=0)
 # %%
 rf = RandomForestClassifier(n_estimators=200, random_state=0)
 rf.fit(X_train, y_train)
+# %%
+y_pred = rf.predict(X_test)
+
+# %%
+
+def plot_confusion_matrix(y_test, y_pred):
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    group_names = ["True Neg","False Pos","False Neg","True Pos"]
+    group_counts = ["{0:0.0f}".format(value) for value in
+                    cf_matrix.flatten()]
+
+    group_percentages = ["{0:.2%}".format(value) for value in
+                        cf_matrix.flatten()/np.sum(cf_matrix)]
+
+    labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in
+            zip(group_names,group_counts,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+
+    sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+    plt.show()
+
+    print("Classification report \n" classification_report(y_test,y_pred))
+    print("Accuracy: ", round(accuracy_score(y_test, y_pred)*100, 4), "%")
+# %%
+plot_confusion_matrix(y_test, y_pred)
+# %%
